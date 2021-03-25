0bf65847f85a20f835f676039820f38abb8c9667
German Brassini <german.brassini@gmail.com>
2021-03-24 22:25:26 -0300

    Creacion primaria de servicios

---
+++ src/app/services/items.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { NgxIndexedDBService } from 'ngx-indexed-db';
+import { Registro } from '../models/registro.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemsService {
+
+  private storeName: string = 'items';
+
+  constructor(private db: NgxIndexedDBService) { }
+
+
+  getAll(){
+    return this.db.getAll(this.storeName);
+  }
+
+  getByKey(key: IDBValidKey){
+    this.db.getByKey(this.storeName, key);
+  }
+
+  create(item: Registro){
+    return this.db.add(this.storeName, item);
+  }
+
+  update(item: Registro){
+    return this.db.update(this.storeName, item);
+  }
+
+  delete(key: IDBValidKey){
+    this.db.delete(this.storeName, key);
+  }
+
+  clean(){
+    this.db.clear(this.storeName);
+  }
+}
