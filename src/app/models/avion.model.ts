29090029cc74bebd1b244ab74bc66e566d17a108
German Brassini <german.brassini@gmail.com>
2021-03-26 19:17:30 -0300

    Modelos de datos - Actualizacion

--- src/app/models/avion.model.ts
+++ src/app/models/avion.model.ts
@@ -1,5 +1,4 @@
-
-export class Avion{
+export class Avion {
   matricula: string;
   n_variable: string;
   c_efectividad: string;
@@ -7,10 +6,27 @@ export class Avion{
   n_linea: string;
   marca: string;
   modelo: string;
+  id?: number;
 
-  constructor(marca: string, modelo: string, matricula: string){
+  constructor(
+    marca: string = "",
+    modelo: string = "",
+    matricula: string = "",
+    n_variable: string = "",
+    c_efectividad: string = "",
+    n_serie: string = "",
+    n_linea: string = "",
+    id?: number
+  ) {
     this.marca = marca;
     this.modelo = modelo;
     this.matricula = matricula;
+    this.n_variable = n_variable;
+    this.c_efectividad = c_efectividad;
+    this.n_serie = n_serie;
+    this.n_linea = n_linea;
+    if(id){
+      this.id = id;
+    }
   }
 }
--- src/app/models/options.model.ts
+++ src/app/models/options.model.ts
@@ -1,40 +1,97 @@
-export class Opciones{
+import { Avion } from "./avion.model";
+export class Opciones {
+  id?: number;
   preliminar: Preliminar[];
   criterios: Criterios[];
   impactos: Impacto[];
   categorias: Categoria[];
 }
 
-
-export class Preliminar{
+export class Preliminar {
+  id?: number;
   nombre: string;
   desc: string;
   opc?: boolean;
-  created_at: string;
+  para_aprobacion: boolean;
+  created_at: Date;
+
+  constructor(
+    nombre: string = "",
+    desc: string = "",
+    para_aprobacion: boolean = false,
+    id?: number
+  ) {
+    this.nombre = nombre;
+    this.desc = desc;
+    this.para_aprobacion = para_aprobacion;
+    this.created_at = new Date();
+    if (id) {
+      this.id = id;
+    }
+  }
 }
 
-export class Criterios{
+export class Criterios {
+  id?: number;
   nombre: string;
   desc: string;
-  created_at: string;
-  pond: number;
-  peso?: number;
-  pp?: number;
+  created_at: Date;
+  pond?: number;
+  peso: number;
+  constructor(
+    nombre: string = "",
+    desc: string = "",
+    peso: number = null,
+    id?: number
+  ) {
+    this.nombre = nombre;
+    this.desc = desc;
+    this.created_at = new Date();
+    this.peso = peso;
+    if (id) {
+      this.id = id;
+    }
+  }
 }
 
-export class Impacto{
-  tipo: 'prog' | 'costos';
+export class Impacto {
+  id?: number;
+  tipo: "prog" | "costos";
   value: number;
-  params: Parametros;
-}
-
-export class Parametros{
   min: number;
   max: number;
 }
 
-export class Categoria{
+export class Categoria {
+  id?: number;
   nombre: string;
+  desc: string;
+  constructor(nombre: string = "", desc: string = "") {
+    this.nombre = nombre;
+    this.desc = desc;
+  }
+  update(nombre: string, desc: string, id: number) {
+    this.nombre = nombre;
+    this.desc = desc;
+    this.id = id;
+  }
+  log() {
+    console.log(this.id, this.nombre, this.desc);
+  }
 }
 
-export type impactType = 'BP' | 'BC' | 'MP' | 'MC' |'AP' | 'AC';
+export type impactType = "BP" | "BC" | "MP" | "MC" | "AP" | "AC";
+
+export type storeNameType =
+  | "criterios"
+  | "preliminares"
+  | "categorias"
+  | "impactos"
+  | "aviones";
+
+export type optionConfigType =
+  | Preliminar
+  | Criterios
+  | Impacto
+  | Categoria
+  | Avion;
