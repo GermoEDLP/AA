7ae97f89999b452c820fb85aefbca11b57419731
German Brassini <german.brassini@gmail.com>
2021-03-27 03:22:08 -0300

    Actualizacion de base de datos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   production: false,
-  environment: 'LOCAL',
+  environment: "LOCAL",
   dbConfig: {
     name: "MyDb",
     version: 1,
@@ -9,8 +9,8 @@ export const AppConfig = {
         store: "registros",
         storeConfig: { keyPath: "id", autoIncrement: true },
         storeSchema: [
-          { name: "name", keypath: "name", options: { unique: false } },
-          { name: "desc", keypath: "desc", options: { unique: false } },
+          { name: "n_doc", keypath: "n_doc", options: { unique: false } },
+          { name: "titulo", keypath: "titulo", options: { unique: false } },
           {
             name: "craeted_at",
             keypath: "craeted_at",
@@ -21,7 +21,18 @@ export const AppConfig = {
             keypath: "updated_at",
             options: { unique: false },
           },
-          { name: "opc1", keypath: "opc1", options: { unique: false } },
+          { name: "c_ata", keypath: "c_ata", options: { unique: false } },
+          { name: "f_emision", keypath: "f_emision", options: { unique: false } },
+          { name: "f_revision", keypath: "f_revision", options: { unique: false } },
+          { name: "categ", keypath: "categ", options: { unique: false } },
+          { name: "n_revision", keypath: "n_revision", options: { unique: false } },
+          { name: "n_ela", keypath: "n_ela", options: { unique: false } },
+          { name: "ela", keypath: "ela", options: { unique: false } },
+          { name: "preliminares", keypath: "preliminares", options: { unique: false } },
+          { name: "aviones", keypath: "aviones", options: { unique: false } },
+          { name: "criterios", keypath: "criterios", options: { unique: false } },
+          { name: "comentarios", keypath: "comentarios", options: { unique: false } },
+          { name: "reunion", keypath: "reunion", options: { unique: false } },
         ],
       },
       {
@@ -73,16 +84,29 @@ export const AppConfig = {
             keypath: "max",
             options: { unique: false },
           },
-          { name: "opc1", keypath: "opc1", options: { unique: false } },
+          { name: "tope", keypath: "tope", options: { unique: false } },
+          { name: "base", keypath: "base", options: { unique: false } },
         ],
       },
       {
         store: "aviones",
         storeConfig: { keyPath: "id", autoIncrement: true },
         storeSchema: [
-          { name: "matricula", keypath: "matricula", options: { unique: false } },
-          { name: "n_variable", keypath: "n_variable", options: { unique: false } },
-          { name: "c_efectividad", keypath: "c_efectividad", options: { unique: false } },
+          {
+            name: "matricula",
+            keypath: "matricula",
+            options: { unique: false },
+          },
+          {
+            name: "n_variable",
+            keypath: "n_variable",
+            options: { unique: false },
+          },
+          {
+            name: "c_efectividad",
+            keypath: "c_efectividad",
+            options: { unique: false },
+          },
           { name: "n_serie", keypath: "n_serie", options: { unique: false } },
           { name: "n_linea", keypath: "n_linea", options: { unique: false } },
           { name: "marca", keypath: "marca", options: { unique: false } },
@@ -94,9 +118,9 @@ export const AppConfig = {
         storeConfig: { keyPath: "id", autoIncrement: true },
         storeSchema: [
           { name: "name", keypath: "name", options: { unique: false } },
-          { name: "desc", keypath: "desc", options: { unique: false } }
+          { name: "desc", keypath: "desc", options: { unique: false } },
         ],
-      }
+      },
     ],
   },
 };
